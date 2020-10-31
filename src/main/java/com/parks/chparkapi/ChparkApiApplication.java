29070c423b3daab2eb23d4f2f6f4fd0879a0470e
chpark <redgum1004@hanmail.net>
2020-10-31 14:59:23 +0900

    init Project

---
+++ src/main/java/com/parks/chparkapi/ChparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.parks.chparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChparkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChparkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parks/chparkapi/ChparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parks.chparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChparkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
