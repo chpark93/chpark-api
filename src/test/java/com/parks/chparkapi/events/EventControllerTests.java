e97297dc1e3b4a34d49a886f5be35234202fe223
chpark <redgum1004@hanmail.net>
2020-11-01 15:09:20 +0900

    validation MVC

--- src/test/java/com/parks/chparkapi/events/EventControllerTests.java
+++ src/test/java/com/parks/chparkapi/events/EventControllerTests.java
@@ -61,4 +61,14 @@ public class EventControllerTests {
                 .andExpect( status().isBadRequest() );
     }
 
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+         EventDTO eventDTO = EventDTO.builder().build();
+
+         this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDTO)))
+                 .andExpect(status().isBadRequest());
+    }
+
 }
